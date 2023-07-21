9eaad6d292bf4ff1189f3f2cf5f0152088c94fb6
Kenneth Solomon <mr.kennethsolomon@gmail.com>
2023-07-21 11:15:45 +0800

    remap float term

--- nvim/lua/user/plugins/floaterm.lua
+++ nvim/lua/user/plugins/floaterm.lua
@@ -1,5 +1,5 @@
-vim.keymap.set('n', '<F1>', ':FloatermToggle scratch<CR>')
-vim.keymap.set('t', '<F1>', '<C-\\><C-n>:FloatermToggle scratch<CR>')
+vim.keymap.set('n', '<leader>t', ':FloatermToggle scratch<CR>')
+vim.keymap.set('t', '<leader>t', '<C-\\><C-n>:FloatermToggle scratch<CR>')
 
 vim.g.floaterm_gitcommit = 'floaterm'
 vim.g.floaterm_autoinsert = 1
